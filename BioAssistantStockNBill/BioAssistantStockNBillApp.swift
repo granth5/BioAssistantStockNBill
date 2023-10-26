c9385c3ff2415a1e18a658fe7a5bef54475944fc
GrantH <grant@ghhsoftware.com>
2023-10-26 18:15:13 -0400

    Initial Commit

---
+++ BioAssistantStockNBill/BioAssistantStockNBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioAssistantStockNBillApp.swift
+//  BioAssistantStockNBill
+//
+//  Created by Grant on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BioAssistantStockNBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
