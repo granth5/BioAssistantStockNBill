9054fd2028e69b24fd8ac25fb2bb32aa966285db
GrantH <grant@ghhsoftware.com>
2023-11-05 20:45:09 -0500

    commit

--- BioAssistantStockNBill/AppData/JsonDataHelper.swift
+++ BioAssistantStockNBill/AppData/JsonDataHelper.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+struct OrderListOrder: Codable, Identifiable {
+    enum CodingKeys: CodingKey {
+        case orderDate
+        case lastName
+        case firstName
+        case insuranceType
+        case insuranceName
+    }
+    
+    var id = UUID()
+    var orderDate: Date
+    var lastName: String
+    var firstName: String
+    var insuranceType: String
+    var insuranceName: String
+
+}
+
+
+class ReadJsonData: ObservableObject {
+    @Published var orders = [OrderListOrder]()
+    
+    init() {
+        loadData()
+    }
+    
+    func loadData() {
+        guard let url = Bundle.main.url(forResource: "OrderData", withExtension: "json")
+        else {
+            print("Json file not found")
+            return
+        }
+        
+        let data = try? Data(contentsOf: url)
+        
+        //let orders = try? JSONDecoder().decode([OrderListOrder].self, from: data!)
+        let decoder = JSONDecoder()
+        //decoder.dateDecodingStrategy = .millisecondsSince1970
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+            do {
+                // process data
+                self.orders = try decoder.decode([OrderListOrder].self, from: data!)
+            } catch let DecodingError.dataCorrupted(context) {
+                print(context)
+            } catch let DecodingError.keyNotFound(key, context) {
+                print("Key '\(key)' not found:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch let DecodingError.valueNotFound(value, context) {
+                print("Value '\(value)' not found:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch let DecodingError.typeMismatch(type, context)  {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {
+                print("error: ", error)
+            }
+    }
+}
--- BioAssistantStockNBill/Views/ContentView.swift
+++ BioAssistantStockNBill/Views/ContentView.swift
@@ -8,7 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var orders = ReadJsonData()
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+
+    
     var body: some View {
+        List(orders.orders) { order in
+            VStack {
+                HStack {
+                    Text(dateFormatter.string(from: order.orderDate))
+                    Spacer()
+                    Text(order.lastName)
+                    Spacer()
+                    Text(order.firstName)
+                    Spacer()
+                    Text(order.insuranceType)
+                    Spacer()
+                    Text(order.insuranceName)
+                    Spacer()
+                    Button("Edit") {
+                        
+                    }
+                    Spacer()
+                    Button("Delete") {
+                        
+                    }
+                }
+            }
+        }
+
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
