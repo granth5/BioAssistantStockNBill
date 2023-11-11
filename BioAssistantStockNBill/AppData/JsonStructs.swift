6b55f22e5071985f86b64ec848afd42cb3015f2b
GrantH <grant@ghhsoftware.com>
2023-11-10 19:41:50 -0500

    saving all - most json init stuff done

---
+++ BioAssistantStockNBill/AppData/JsonStructs.swift
@@ -0,0 +1,8 @@
+//
+//  JsonStructs.swift
+//  BioAssistantStockNBill
+//
+//  Created by Grant on 11/10/23.
+//
+
+import Foundation
---
+++ BioAssistantStockNBill/DataModel/CDDataController.swift
@@ -0,0 +1,8 @@
+//
+//  CDDataController.swift
+//  BioAssistantStockNBill
+//
+//  Created by Grant on 11/10/23.
+//
+
+import Foundation
