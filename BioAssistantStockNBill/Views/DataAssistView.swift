174ffed6265eabd820c5be4c30786e764c8fd954
GrantH <grant@ghhsoftware.com>
2023-11-19 19:23:40 -0500

    Saving all

---
+++ BioAssistantStockNBill/Views/DataAssistView.swift
@@ -0,0 +1,18 @@
+//
+//  DataAssistView.swift
+//  BioAssistantStockNBill
+//
+//  Created by Grant on 11/11/23.
+//
+
+import SwiftUI
+
+struct DataAssistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataAssistView()
+}
---
+++ BioAssistantStockNBill/Views/OrderListView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderListView.swift
+//  BioAssistantStockNBill
+//
+//  Created by Grant on 11/17/23.
+//
+
+import SwiftUI
+
+struct OrderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderListView()
+}
